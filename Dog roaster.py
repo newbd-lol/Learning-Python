6439d5fbd51d02948565f90a04ba655532cc0810
newbd <super.ark81@gmail.com>
2026-04-17 09:58:19 -0700

    Add dog roaster script for user interaction
    Create a script that interacts with the user about their dog.

---
+++ Dog roaster.py
@@ -0,0 +1,19 @@
+import time
+
+dog_name = input("What is ur dogs name?????:")
+breed = input("What breed is ur doggggg????:")
+print(f"SON ur dog breed is {breed} ur dog is BADDD BOIIII dude if ur dog is a {breed} i cant even IMAGINE having that dog broooo AND on top of that u named it {dog_name}")
+print("Just WATCH me boi")
+time.sleep(3)
+print("HERE IT COMESSSSSSSS")
+time.sleep(4)
+print(3)
+time.sleep(1)
+print(2)
+time.sleep(1)
+print(1)
+time.sleep(1)
+print("RIP ur computer lol")
+time.sleep(1)
+for i in range(900):
+    print(dog_name)
