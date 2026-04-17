f407375e01c61714e4d1b2b61f8621431ca87335
newbd <super.ark81@gmail.com>
2026-04-17 13:29:14 -0700

    Add roast_dog function to interact with user
    Took 5 minutes to learn im pretty sure this is a function

--- function.py
+++ function.py
@@ -1,1 +1,6 @@
-
+def roast_dog(): 
+    name = input("What is your dogs name?:")
+    age = input("How old is your dog?:")
+    print (f"Imagine naming ur dog {name} and having it be {age} old IMAGINEEEE")
+    
+roast_dog()
